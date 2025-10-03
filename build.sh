9853fdf5b998d0fab533d51b4df72d2b4ea4ea72
Saurabh Mourya <mouryasaurabh9936@gmail.com>
2025-10-03 19:42:50 +0530

    new-8

--- build.sh
+++ build.sh
@@ -10,5 +10,5 @@ python manage.py collectstatic --noinput
 # Apply migrations
 python manage.py migrate --noinput
 
-# Start Gunicorn server
+# Gunicorn is not needed for Vercel; Vercel runs the app via its own handler
 gunicorn django_pro.wsgi:application --bind 0.0.0.0:$PORT
