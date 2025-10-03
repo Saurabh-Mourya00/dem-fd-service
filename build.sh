f03546368feb25f83d8daefa2e9e218969a2edd7
Saurabh Mourya <mouryasaurabh9936@gmail.com>
2025-10-03 18:55:10 +0530

    new-5

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+set -e
 
 # Install dependencies
 pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --noinput
+
+# Apply migrations
+python manage.py migrate --noinput
+
+# Start Gunicorn server
+gunicorn django_pro.wsgi:application --bind 0.0.0.0:$PORT
