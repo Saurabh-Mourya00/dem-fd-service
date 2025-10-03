3cb36f192fded4a9d83d2db81d8120a6588356c0
Saurabh Mourya <mouryasaurabh9936@gmail.com>
2025-10-03 17:41:23 +0530

    new-1

--- build.sh
+++ build.sh
@@ -9,5 +9,8 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --noinput
 
-# Apply database migrations (uncomment the line below if you want to run migrations on each deployment)
-# python manage.py migrate
+# Apply database migrations
+python manage.py migrate --noinput
+
+# Start Gunicorn server
+gunicorn django_pro.wsgi:application --bind 0.0.0.0:$PORT
